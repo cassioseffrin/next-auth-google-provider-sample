da5ab980b658147dea21a01ec5ca158a903e355f
Cassio Seffrin <cassioseffrin@gmail.com>
2023-11-16 15:37:26 -0300

    img

--- src/app/components/UserInfo.jsx
+++ src/app/components/UserInfo.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Image from "next/image";
+// import Image from "next/image";
 import SigninButton from "./SigninButton";
 import { useSession } from "next-auth/react";
 
@@ -10,7 +10,7 @@ export   function UserInfo() {
   if (status === "authenticated") {
     return (
       <div className="shadow-xl p-8 rounded-md flex flex-col gap-3 bg-grey-600">
-        <Image
+        <img
           className="rounded-full"
           src={session?.user?.image ?? 'https://png.pngtree.com/png-clipart/20190520/original/pngtree-vector-users-icon-png-image_4144740.jpg'}
           width={60}
