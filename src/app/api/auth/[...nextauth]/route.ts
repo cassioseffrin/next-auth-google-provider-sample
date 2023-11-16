1b79ae2618d44278ed682b3df209096426f6a61e
Cassio Seffrin <cassioseffrin@gmail.com>
2023-11-16 14:45:05 -0300

    cache

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,56 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+const handler = NextAuth({
+  logger: {
+    error(code, metadata) {
+      console.error(code, metadata);
+    },
+    warn(code) {
+      console.warn(code);
+    },
+    debug(code, metadata) {
+      console.debug(code, metadata);
+    },
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+ 
+
+        const res = await fetch("https://api.repag.com.br/auth/signin?domain=provin", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: credentials?.username,
+            password: credentials?.password,
+            domain: "provin" 
+          }),
+        });
+        const user = await res.json();
+
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  pages:{signIn:'/signin'},
+  secret: 'asdfasdlfkjsdklfjsdlk345345'
+});
+
+export { handler as GET, handler as POST };
