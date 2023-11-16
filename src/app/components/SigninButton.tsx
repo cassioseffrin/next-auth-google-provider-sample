9c3f559c03ff0427db2ea4e29f4e8e7622342b7e
Cassio Seffrin <cassioseffrin@gmail.com>
2023-11-16 15:24:18 -0300

    ok

--- src/app/components/SigninButton.tsx
+++ src/app/components/SigninButton.tsx
@@ -9,14 +9,14 @@ const SigninButton = () => {
       <div className="flex gap-4 ml-auto">
         <p className="text-sky-600">{session.user.name}</p>
         <button onClick={() => signOut()} className="text-red-600">
-          Sign Out
+          Sair
         </button>
       </div>
     );
   }
   return (
     <button onClick={() => signIn()} className="text-green-600 ml-auto">
-      Sign In
+      Logar
     </button>
   );
 };
