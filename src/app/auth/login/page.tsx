5d0cb4c1d1fe557dc4f8e29868468aba28bdbd91
Cassio Seffrin <cassioseffrin@gmail.com>
2023-11-17 16:13:32 -0300

    format

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,65 +1,60 @@
- 
- "use client";
- 
+"use client";
+
 import Button from "@/app/components/elements/Button";
 import TextBox from "@/app/components/elements/TextBox";
- 
- import React, { useEffect, useRef, useState } from "react";
- import { GetSessionParams, getProviders, getSession, signIn } from "next-auth/react"
-import { GetServerSidePropsContext } from "next";
-import { getServerSession } from "next-auth";
- const LoginPage = () => {
-   const userName = useRef("");
-   const pass = useRef("");
-   const [providers, setProviders] = useState({});
+import React, { useEffect, useRef, useState } from "react";
+import { getProviders, signIn } from "next-auth/react";
 
-   useEffect(() => {
-     (async () => {
-       const res = await getProviders() as any;
-       setProviders(res);
-     })();
-   }, []);
+const LoginPage = () => {
+  const userName = useRef("");
+  const pass = useRef("");
+  const [providers, setProviders] = useState({});
 
-   const onSubmit = async () => {
-     const result = await signIn("credentials", {
-       username: userName.current,
-       password: pass.current,
-       redirect: true,
-       callbackUrl: "/",
-     });
-   };
-   return (
-     <div
-       className={
-         "flex flex-col justify-center items-center  h-screen bg-gradient-to-br gap-1 from-cyan-300 to-sky-600"
-       }
-     >
-       <div className="px-7 py-4 shadow bg-white rounded-md flex flex-col gap-2">
+  useEffect(() => {
+    (async () => {
+      const res = (await getProviders()) as any;
+      setProviders(res);
+    })();
+  }, []);
 
-         <TextBox
-           labelText="email"
-           value={'cassioseffrin@gmail.com'}
-           onChange={(e) => (userName.current = e.target.value)}
-         />
-         <TextBox
-           labelText="senha"
-           type={"password"}
-           onChange={(e) => (pass.current = e.target.value)}
-         />
-         <Button onClick={onSubmit}>Entrar</Button>
-       </div>
-       <>
-      {Object.values(providers).map((provider: any) => (
-        <div key={provider.name}>
-          <button onClick={() => signIn(provider.id)}>
-            Logar com:  {provider.name}
-          </button>
-        </div>
-      ))}
-    </>
-     </div>
-   );
- };
- 
- export default LoginPage;
+  const onSubmit = async () => {
+    const result = await signIn("credentials", {
+      username: userName.current,
+      password: pass.current,
+      redirect: true,
+      callbackUrl: "/",
+    });
+  };
+  return (
+    <div
+      className={
+        "flex flex-col justify-center items-center  h-screen bg-gradient-to-br gap-1 from-cyan-300 to-sky-600"
+      }
+    >
+      <div className="px-7 py-4 shadow bg-white rounded-md flex flex-col gap-2">
+        <TextBox
+          labelText="email"
+          value={"cassioseffrin@gmail.com"}
+          onChange={(e) => (userName.current = e.target.value)}
+        />
+        <TextBox
+          labelText="senha"
+          type={"password"}
+          onChange={(e) => (pass.current = e.target.value)}
+        />
+        <Button onClick={onSubmit}>Entrar</Button>
+      </div>
+      <>
+        {Object.values(providers).map((provider: any) => (
+          <div key={provider.name}>
+            <button onClick={() => signIn(provider.id)}>
+              Logar com: {provider.name}
+            </button>
+          </div>
+        ))}
+      </>
+    </div>
+  );
+};
 
+export default LoginPage;
